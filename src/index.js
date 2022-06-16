3c3f56b79bf284c468e0d4188da6f1e137530500
Leonard Scheidemantel <leonard@lsindustries.de>
2022-06-16 18:14:11 +0200

    🎉 Initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import {JsonRpc} from "eosjs";
+import fetch from "node-fetch";
+
+const token = "YOUR_TOKEN_HERE"
+const endpoint = "https://pro.wax.oneapi.dev"
+
+/**
+ * Create rpc with Bearer token
+ */
+const rpc = new JsonRpc(endpoint, {
+    fetch: ((input, init) =>
+            fetch(input, {...init, headers: {
+                    "Authorization": "Bearer " + token
+                }})
+    ),
+});
+
+/**
+ * Will log /v1/chain/get_info request if authentication is working
+ * Will log "invalid json response body at ... " if authentication failed
+ */
+rpc.get_info().then(result => console.log(result))
